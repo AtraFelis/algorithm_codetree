ef31479574cfec09e244f7fc800226f2afb7764b
AtraFelis <107051266+AtraFelis@users.noreply.github.com>
2025-06-25 16:00:21 +0900

    Title: 행복한 수열의 개수, Time: 118ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250625/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+  
+public class Main {
+    public static int ans;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        
+        ans = 0;
+        checkRow(grid, m);
+        checkCol(grid, m);
+        System.out.println(ans);
+    }
+
+    private static void checkRow(int[][] grid, int m) {
+        int len = grid.length;
+        for(int i=0; i<len; i++) {
+            int check = 1;    
+            for(int j=1; j<len; j++) {
+                if(grid[i][j-1]==grid[i][j]) check++;
+                else check = 1;
+
+                if(check >= m) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void checkCol(int[][] grid, int m) {
+        int len = grid.length;
+        for(int i=0; i<len; i++) {
+            int check = 1;    
+            for(int j=1; j<len; j++) {
+                if(grid[j-1][i]==grid[j][i]) check++;
+                else check = 1;
+
+                if(check >= m) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+    }
+}
