5c3c5acf34ad697b216849868f84c1f920d939f0
AtraFelis <107051266+AtraFelis@users.noreply.github.com>
2025-08-06 15:12:02 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 131ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 250806/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.java
+++ 250806/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.java
@@ -3,13 +3,16 @@ import java.util.*;
 public class Main {
 
     private static List<Integer[]> pairs = new ArrayList<>();
+    public static int[] pair;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
         int n = sc.nextInt();
 
-        backtracking(k, 0);
+        pair = new int[n];
+
+        backtracking(k, n, 0);
         StringBuilder sb = new StringBuilder();
         for(Integer[] pair : pairs) {
             sb.append(pair[0]).append(" ").append(pair[1]).append('\n');
@@ -17,17 +20,15 @@ public class Main {
 
         System.out.println(sb);
     }
-
-    public static int[] pair = new int[2];
-    public static void backtracking(int k, int depth) {
-        if(depth == 2) {
+    public static void backtracking(int k, int n, int depth) {
+        if(depth == n) {
             pairs.add(new Integer[]{pair[0], pair[1]});
             return;
         }
 
         for(int i = 1; i <= k; i++) {
             pair[depth] = i;
-            backtracking(k, depth+1);
+            backtracking(k, n, depth+1);
         }
     }
 }
