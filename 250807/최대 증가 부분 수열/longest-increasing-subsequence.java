6cec02a9e2cba78a089f7e90f7f2ca56307a3aef
AtraFelis <107051266+AtraFelis@users.noreply.github.com>
2025-08-07 21:01:07 +0900

    Title: 최대 증가 부분 수열, Time: 227ms, Memory: 13MB, Status: Passed - Codetree

---
+++ 250807/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int[] dp = new int[n]; // 해당 인덱스를 시작점으로 하는 가장 긴 증가하는 부분 수열의 길이를 저장
+
+        if(n == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        Arrays.fill(dp, 1);
+        for(int i = n-2; i >= 0; i--) {
+            for(int j = i + 1; j < n; j++) {    
+                if(arr[i] < arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int answer = 0;
+        for(int i : dp) {
+            answer = Math.max(answer, i);
+        }
+
+        System.out.println(answer);
+    }
+}
