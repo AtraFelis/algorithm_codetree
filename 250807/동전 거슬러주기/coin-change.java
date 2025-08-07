10693a786a46e4261d48460ac88dd1f58a69d5aa
AtraFelis <107051266+AtraFelis@users.noreply.github.com>
2025-08-07 21:45:58 +0900

    Title: 동전 거슬러주기, Time: 83ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250807/동전 거슬러주기/coin-change.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+
+        int[] coin = new int[n];
+        Arrays.sort(coin);
+
+        stk = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++)
+            coin[i] = Integer.parseInt(stk.nextToken());
+        
+        int[] dp = new int[m + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        for (int i = 0; i < n; i++)
+            if(coin[i] <= m)
+                dp[coin[i]] = 1;
+        
+        for(int i = coin[0]; i <= m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i + coin[j] > m) continue;
+                dp[i + coin[j]] = Math.min(dp[i + coin[j]], dp[i] + 1);
+            }
+        }
+
+        int answer = dp[m] != Integer.MAX_VALUE ? dp[m] : -1;
+        System.out.println(answer);
+    }
+}
