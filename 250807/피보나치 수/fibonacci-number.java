f383e787492d2591c0d904ae9be99907f04d90c5
AtraFelis <107051266+AtraFelis@users.noreply.github.com>
2025-08-07 20:05:14 +0900

    Title: 피보나치 수, Time: 126ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 250807/피보나치 수/fibonacci-number.java
+++ 250807/피보나치 수/fibonacci-number.java
@@ -8,7 +8,7 @@ public class Main {
         dp[1] = 1;
         dp[2] = 1;
 
-        for(int i = 2; i <= n; i++) {
+        for(int i = 3; i <= n; i++) {
             dp[i] = dp[i-1] + dp[i-2];
         }
 
