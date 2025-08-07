7d09a5d9c0429e2f8220de4d73b7e2fafeba6a67
AtraFelis <107051266+AtraFelis@users.noreply.github.com>
2025-08-07 11:28:38 +0900

    Title: 강력한 폭발, Time: 144ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250807/강력한 폭발/strong-explosion.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+public class Main {
+
+    private static int n;
+    private static int[][] grid;
+    private static List<int[]> pos = new ArrayList<>();
+
+    private static int answer = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        grid = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+
+                if(grid[i][j] == 1)
+                    pos.add(new int[]{i, j});
+            }
+        }
+
+        int[] batch = new int[pos.size()];
+        backtracking(batch, 0);
+
+        System.out.println(answer);
+    }
+
+    private static void backtracking(int[] batch, int depth) {
+        if(depth == pos.size()) {
+            boolean[][] map = new boolean[n][n];
+            for(int i = 0; i < pos.size(); i++) {
+                int[] curPos = pos.get(i);
+                map[curPos[0]][curPos[1]] = true;
+
+                if(batch[i] == 0) {
+                    map[curPos[0] - 2 >= 0 ? curPos[0] - 2 : curPos[0]][curPos[1]] = true;
+                    map[curPos[0] - 1 >= 0 ? curPos[0] - 1 : curPos[0]][curPos[1]] = true;
+                    map[curPos[0] + 1 < n ? curPos[0] + 1 : curPos[0]][curPos[1]] = true;
+                    map[curPos[0] + 2 < n ? curPos[0] + 2 : curPos[0]][curPos[1]] = true;
+                } else if(batch[i] == 1) {
+                    map[curPos[0] - 1 >= 0 ? curPos[0] - 1 : curPos[0]][curPos[1]] = true;
+                    map[curPos[0]][curPos[1] + 1 < n ? curPos[1] + 1 : curPos[1]] = true;
+                    map[curPos[0] + 1 < n ? curPos[0] + 1 : curPos[0]][curPos[1]] = true;
+                    map[curPos[0]][curPos[1] - 1 >= 0 ? curPos[1] - 1 : curPos[1]] = true;
+                } else {
+                    map[curPos[0] - 1 >= 0 ? curPos[0] - 1 : curPos[0]][curPos[1] - 1 >= 0 ? curPos[1] - 1 : curPos[1]] = true;
+                    map[curPos[0] - 1 >= 0 ? curPos[0] - 1 : curPos[0]][curPos[1] + 1 < n ? curPos[1] + 1 : curPos[1]] = true;
+                    map[curPos[0] + 1 < n ? curPos[0] + 1 : curPos[0]][curPos[1] + 1 < n ? curPos[1] + 1 : curPos[1]] = true;
+                    map[curPos[0] + 1 < n ? curPos[0] + 1 : curPos[0]][curPos[1] - 1 >= 0 ? curPos[1] - 1 : curPos[1]] = true;
+                }
+            }
+
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if(map[i][j]) cnt++;
+                }
+            }
+
+            answer = Math.max(answer, cnt);
+            return;
+        }
+        for(int i = 0; i < 3; i++) {
+            batch[depth] = i;
+            backtracking(batch, depth + 1);
+        }
+    }
+}
