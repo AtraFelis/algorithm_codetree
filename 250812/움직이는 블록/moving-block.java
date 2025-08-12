1c2301e00b8f1f9a63fb30dac7f370f42c653d5e
AtraFelis <107051266+AtraFelis@users.noreply.github.com>
2025-08-12 09:35:34 +0900

    Title: 움직이는 블록, Time: 311ms, Memory: 21MB, Status: Passed - Codetree

---
+++ 250812/움직이는 블록/moving-block.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] blocks = new int[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            blocks[i] = sc.nextInt();
+            sum += blocks[i];
+        }
+        int avg = sum/n;
+
+        int answer = 0;
+        for(int i = 0; i < n; i++) {
+            answer += blocks[i] - avg > 0 ? blocks[i] - avg : 0;
+        }
+
+        System.out.println(answer);
+    }
+}
