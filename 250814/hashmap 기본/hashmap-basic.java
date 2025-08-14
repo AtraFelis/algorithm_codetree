c1f350e44aa8dbc38818b64c689c3c36a3a0e0b2
AtraFelis <107051266+AtraFelis@users.noreply.github.com>
2025-08-14 20:51:26 +0900

    Title: hashmap 기본, Time: 355ms, Memory: 21MB, Status: Passed - Codetree

---
+++ 250814/hashmap 기본/hashmap-basic.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            String command = stk.nextToken();
+
+            if(command.equals("add")){
+                int key = Integer.parseInt(stk.nextToken());
+                int value = Integer.parseInt(stk.nextToken());
+                map.put(key, value);
+            }
+            else if(command.equals("remove")){
+                int key = Integer.parseInt(stk.nextToken());
+                map.remove(key);
+            }
+            else {
+                int key = Integer.parseInt(stk.nextToken());
+                sb.append(map.containsKey(key) ? map.get(key) : "None").append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
